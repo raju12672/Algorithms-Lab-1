c4333d26b97d93aeeb0de0da061f029a46864b21
Albin <albinhossain@outlook.com>
2020-06-29 08:56:29 +0600

    Implemented

--- Lab Tasks/Week 4/Sherlock-and-Numbers.cpp
+++ Lab Tasks/Week 4/Sherlock-and-Numbers.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int n, k, p, x;
-
 int main() {
-    int tests;
+    int tests, n, k, p, x;
     cin >> tests;
     while (tests--) {
         cin >> n >> k >> p;
